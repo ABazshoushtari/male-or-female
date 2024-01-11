68547f522aaf1e35f441162b6a3bb1135dc4c943
Ali Bazshoushtari <abazshoushtari@gmail.com>
2024-01-12 00:18:39 +0330

    Implement clearing saved answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,9 @@
-var submitButton = document.getElementById("submit");
-var saveButton = document.getElementById("save");
-
 // Error handling for name input. Validating the input and form. Showing error for unvalid conditions.
 document.getElementById("nameInput").addEventListener("input", function(e) {
     var validInput = /^[a-zA-Z\s]*$/;
     var errorMessageElement = document.getElementById("error");
-    // var submitButton = document.getElementById("submit");
-    // var saveButton = document.getElementById("save");
+    var submitButton = document.getElementById("submit");
+    var saveButton = document.getElementById("save");
 
     if (!validInput.test(e.target.value) || e.target.value === "") {
         // Display an error message and disable buttons
@@ -77,3 +74,14 @@ document.getElementById("form").onsubmit = function(event) {
             console.error("Error fetching data: ", error);
         });
 };
+
+
+document.getElementById("clear-saved").addEventListener("click", function() {
+    var name = document.getElementById("nameInput").value.trim();
+    var savedGender = localStorage.getItem(name);
+
+    if (savedGender) {
+        localStorage.removeItem(name);
+        document.getElementById("saved-answer").textContent = name + "'s saved info got removed."
+    }
+});
